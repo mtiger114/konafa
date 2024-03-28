41911c65a64855bd8655e52833e51ec436501c65
Mahmoud Fahmy <105823344+Ma7moudTiger@users.noreply.github.com>
2024-03-28 06:23:38 +0200

    a

--- js/konafa.js
+++ js/konafa.js
@@ -1,18 +1,72 @@
 function findColorAndVegetable() {
-  const colors = ["متشنكحه", "متغرقة", "متدلعة", "متحزمة", "متبعترة", "مسخسخة", "مزفلطة", "مفرهدة", "ملظلظة", "مدروخة", "منحنجه", "متنكده", "مهبطة", "متمرمطة", "مقريفة", "هفتانه", "زهقانة", "حرانة", "متروقة", "عيوطة", "مهبودة", "مفرهدة", "متمرمطة", "مسقسقة", "سهتانة", "معكننه", "ملزقة", "مسحولة", "مسنجلة", "مبهدلة", "تخنانة"];
-  const vegetables = ["بالمهلبية", "بالقشطة", "بالمكسرات", "بالزعتر", "بالكستر", "بالجيلي", "بالبلح", "بالعسل", "بالكراميل", "بالجبنة", "بالزبادي", "بالشربات"];
+  const colors = [
+    "متشنكحه",
+    "متغرقة",
+    "متدلعة",
+    "متحزمة",
+    "متبعترة",
+    "مسخسخة",
+    "مزفلطة",
+    "مفرهدة",
+    "ملظلظة",
+    "مدروخة",
+    "منحنجه",
+    "متنكده",
+    "مهبطة",
+    "متمرمطة",
+    "مقريفة",
+    "هفتانه",
+    "زهقانة",
+    "حرانة",
+    "متروقة",
+    "عيوطة",
+    "مهبودة",
+    "مفرهدة",
+    "متمرمطة",
+    "مسقسقة",
+    "سهتانة",
+    "معكننه",
+    "ملزقة",
+    "مسحولة",
+    "مسنجلة",
+    "مبهدلة",
+    "تخنانة",
+  ];
+  const vegetables = [
+    "بالمهلبية",
+    "بالقشطة",
+    "بالمكسرات",
+    "بالزعتر",
+    "بالكستر",
+    "بالجيلي",
+    "بالبلح",
+    "بالعسل",
+    "بالكراميل",
+    "بالجبنة",
+    "بالزبادي",
+    "بالشربات",
+  ];
 
   const input = document.getElementById("birthday").value;
   const birthday = input.split("/");
   const day = parseInt(birthday[0]);
   const month = parseInt(birthday[1]);
 
-  if (isNaN(day) || isNaN(month) || day < 1 || day > 31 || month < 1 || month > 12) {
-      showError("Invalid date. Please enter a valid date (DD/MM).");
+  if (
+    isNaN(day) ||
+    isNaN(month) ||
+    day < 1 ||
+    day > 31 ||
+    month < 1 ||
+    month > 12
+  ) {
+    showError("اكتبها صح (يوم / شهر)");
   } else {
-      const color = colors[day - 1];
-      const vegetable = vegetables[month - 1];
-      showResult(`You are <span style="color:${color.toLowerCase()}">${color}</span> - ${vegetable}`);
+    const color = colors[day - 1];
+    const vegetable = vegetables[month - 1];
+    showResult(
+      `You are <span style="color:${color.toLowerCase()}">${color}</span> - ${vegetable}`
+    );
   }
 }
 
